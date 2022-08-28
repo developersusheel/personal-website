0bbb674b57f2f2dd2959e288fc44ddeb90f96755
Susheel Kumar <susheel.kumar.msl@gmail.com>
2022-08-28 19:48:18 +0530

    Boilerplate setup completed

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className="text-6xl font-bold">Test</h1>
+
+      <button>Submit</button>
+    </div>
+  )
+}
+
+export default Home
